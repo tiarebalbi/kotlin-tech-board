7ccb048058f2b0312301f9de84e1b5ac809b78a1
Tiare Balbi Bonamini <tiare.balbi@mttnow.com>
2017-03-06 11:20:01 +0000

    Improving tests

--- src/test/kotlin/com/tiarebalbi/repository/UserRepositoryTest.kt
+++ src/test/kotlin/com/tiarebalbi/repository/UserRepositoryTest.kt
@@ -3,41 +3,86 @@ package com.tiarebalbi.repository
 import com.tiarebalbi.AbstractIntegrationTests
 import com.tiarebalbi.model.User
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.fail
 import org.junit.Before
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.mongodb.core.query.Query
+import reactor.test.StepVerifier
 
 
 class UserRepositoryTest : AbstractIntegrationTests() {
 
-    @Autowired
-    lateinit var repository: UserRepository
+  @Autowired
+  lateinit var repository: UserRepository
 
-    @Before
-    fun setUp() {
-        this.repository.deleteAll()
-    }
+  @Before
+  fun setUp() {
+    this.repository.deleteAll()
+  }
 
-    @Test
-    fun `Save a new user in the database`() {
+  @Test
+  fun `Save a new user in the database`() {
+    // given
+    val data = getUser()
 
-        val user = User("me@tiarebalbi.com", "Tiare")
-        this.repository.save(user)
-                .doOnError { fail("Error to save the user") }
-                .doOnSubscribe { assertThat(user.email).isEqualTo("me@tiarebalbi.com") }
-                .subscribe()
+    // when
+    val user = this.repository.save(data)
 
-    }
+    // then
+    StepVerifier.create(user)
+      .consumeNextWith {
+        assertThat(it.email).isEqualTo("me@tiarebalbi.com")
+        assertThat(it.firstName).isEqualTo("Tiare")
+      }
+      .verifyComplete()
+  }
 
-    @Test
-    fun `Should find the user by his email`() {
-        this.repository.save(User("me@tiarebalbi.com", "Tiare"))
-                .doOnSuccess {
-                    val search = this.repository.findOne("me@tiarebalbi.com")
-                    val result = search.subscribe().peek()
-                    assertThat(result).isNotNull()
-                    assertThat(result.email).isEqualTo("me@tiarebalbi.com")
-                }
-    }
+  @Test
+  fun `Should find the user by his email`() {
+    // given
+    this.repository.save(getUser())
+
+    // when
+    val user = this.repository.findOne("me@tiarebalbi.com")
+
+    // then
+    StepVerifier.create(user)
+      .consumeNextWith {
+        assertThat(it).isNotNull()
+        assertThat(it.email).isEqualTo("me@tiarebalbi.com")
+      }
+      .verifyComplete()
+  }
+
+  @Test
+  fun `Should delete all user when necessary`() {
+    // given
+    this.repository.save(getUser())
+
+    // when
+    this.repository.deleteAll()
+
+    // then
+    StepVerifier.create(this.repository.count(Query()))
+      .expectNextCount(0)
+      .verifyComplete()
+  }
+
+  fun `Should find all records in the database`() {
+    // given
+    this.repository.save(getUser())
+
+    // when
+    val data = this.repository.findAll()
+
+    // then
+    StepVerifier.create(data)
+      .consumeRecordedWith {
+        assertThat(it).isNotNull()
+        assertThat(it.size).isEqualTo(1)
+      }
+      .verifyComplete()
+  }
+
+  private fun getUser() = User("me@tiarebalbi.com", "Tiare")
 }
